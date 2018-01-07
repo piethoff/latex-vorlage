9972c4efafbdc1153ee16b7f63f785da9249c042
Piet Hoffmann <hoffmannpiet42@gmail.com>
2018-01-07 21:44:56 +0100

    mathemann

--- plot.py
+++ plot.py
@@ -8,9 +8,9 @@ mpl.rcParams.update({
     'pgf.preamble': r'\usepackage{siunitx}',
 })
 
-x = np.linspace(0, 10, 80)
+x = np.linspace(0, 10, 20)
 y = x ** np.sin(x)
 
-mp(x,y, dpi=300, factor=60)
+mp(x,y, dpi=300, factor=160)
 
 plt.savefig("1.pdf", transparent=True)
